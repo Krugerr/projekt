f31434f053ac28d3b8a36ea6a4f6086119c61dc1
Krugerr <oziemski.grzegorz@gmail.com>
2017-09-24 14:48:34 +0200

    koleeeejne

---
+++ pierwsza_apka/app/src/main/java/pl/grzegorz/pierwsza_apka/zadanie10_activity.java
@@ -0,0 +1,35 @@
+package pl.grzegorz.pierwsza_apka;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+/**
+ * Created by uczen on 2017-09-24.
+ */
+
+public class zadanie10_activity extends AppCompatActivity {
+
+    ImageView image;
+    ImageButton left;
+    ImageButton right;
+    TextView IndicatorTv;
+    int idicator = 0;
+
+    LinkedList<
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zadanie_10);
+
+
+
+
+
+
+}
