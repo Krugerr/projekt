febcd5f8cbfdc7b1c05b11a8b0b60082b2c65a84
Krugerr <oziemski.grzegorz@gmail.com>
2017-09-24 08:35:41 +0200

    projekt szosty i piaty

--- pierwsza_apka/app/src/main/java/pl/grzegorz/pierwsza_apka/MainActivity.java
+++ pierwsza_apka/app/src/main/java/pl/grzegorz/pierwsza_apka/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.zadanie_czwarte);
+        setContentView(R.layout.zadanie_piate);
     }
 }
